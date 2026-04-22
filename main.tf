6a4baef176800e48f79318c30a8b97274aa724d3
coopervdavis <153779783+coopervdavis@users.noreply.github.com>
2026-04-22 17:28:00 -0600

    infra updates

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ module "vpc" {
   public_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
 
   # For RDS
-  database_subnets             = ["10.0.201.0/24", "10.0.202.0/24"]
-  create_database_subnet_group = true
+  database_subnets                   = ["10.0.201.0/24", "10.0.202.0/24"]
+  create_database_subnet_group       = true
+  create_database_subnet_route_table = true
 }
--- storage.tf
+++ storage.tf
@@ -1,3 +1,8 @@
+# Add to the top of storage.tf
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
 resource "aws_s3_bucket" "media" {
   bucket = "nhl-django-media-assets-${random_id.suffix.hex}"
 }
@@ -40,4 +45,18 @@ resource "aws_cloudfront_distribution" "cdn" {
   viewer_certificate {
     cloudfront_default_certificate = true
   }
+}
+
+# Add to the bottom of storage.tf
+
+resource "aws_s3_object" "team_logos" {
+  for_each = fileset("${path.module}/media/team_logos", "*")
+
+  bucket       = aws_s3_bucket.media.id
+  key          = "team_logos/${each.value}"
+  source       = "${path.module}/media/team_logos/${each.value}"
+  content_type = "image/png"
+
+  # Ensure the bucket exists before trying to upload
+  depends_on = [aws_s3_bucket.media]
 }
